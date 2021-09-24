d3ae6a4b95f6c285aca000daadf2c3e2e754bb66
Nikita <Tomskrenta@mail.ru>
2021-09-25 10:30:35 +1200

    The first
    The first

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AbsolutelyNewNasa
+//
+//  Created by Nikita Shvad on 25.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //Тут я типа написал всякую основу
+        
+    }
+
+
+}
+
