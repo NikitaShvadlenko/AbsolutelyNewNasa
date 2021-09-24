7fd7cc49fa2acd6bf7e48b16870175f78afd3828
Nikita <Tomskrenta@mail.ru>
2021-09-25 10:35:49 +1200

    а1
    а1

--- ViewController.swift
+++ ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         //Тут я типа написал всякую основу
         
+        
+        
+        //Эта ветка выглядит круче, сейчас она будет очень длинной
     }
 
 
