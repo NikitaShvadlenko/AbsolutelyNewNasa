23ad3bc1d6fecaa64bc7af035ae54c7bf38ba2b2
Nikita <Tomskrenta@mail.ru>
2021-09-26 10:27:21 +1200

    image provider request
    something

--- OpenNasaRoute.swift
+++ OpenNasaRoute.swift
@@ -6,4 +6,48 @@
 //
 
 import Foundation
+import Moya
+
+//Enum тут на случай, если я захочу чем-нибудь еще воспользоваться из наса АПИ
+enum OpenNasaRoute {
+    case apod
+}
+
+extension OpenNasaRoute: TargetType {
+    
+    var baseURL: URL {
+        //Как URL Вобще может быть Optional, если я туда Литерал пишу?
+        URL(string: "https://api.nasa.gov/planetary/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .apod:
+            return "apod"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .apod:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .apod:
+            let parameters: [String: Any] = [
+                "api_key": "DEMO_KEY"
+            ]
+            // не запомнил, но понимаю, почему тут такой тип кодировки.
+            return .requestParameters(parameters: parameters, encoding: URLEncoding.queryString)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+}
 
--- PictureCell.swift
+++ PictureCell.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import Moya
 
 class PictureCell: UITableViewCell {
    
@@ -16,7 +17,8 @@ class PictureCell: UITableViewCell {
         return imageOfTheWeek
     }()
     
-    
+    //Предположу, что MoyaProvider просто помогает проще делать try и все такие вещи, связанные с реквестом
+    let imageProvider = MoyaProvider<NasaImageRoute>()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -32,6 +34,10 @@ private extension PictureCell {
         contentView.addSubview(imageOfTheWeek)
         imageOfTheWeek.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+            //Я должен как-то связать NasaImageRoute и OpenNasaRoute Почему url:url?
+            imageProvider.request(.image(url: url)) { <#Result<Response, MoyaError>#> in
+                <#code#>
+            }
         }
     }
 }
